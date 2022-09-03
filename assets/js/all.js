3fbb14d518e84881f71d15171d24aa372466c581
Peter <guanwei0702@gmail.com>
2022-09-03 15:34:34 +0800

    Update 2022-09-03T07:34:05.633Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,16 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  mousewheel: true,
+  keyboard: true
+});
 //# sourceMappingURL=all.js.map
