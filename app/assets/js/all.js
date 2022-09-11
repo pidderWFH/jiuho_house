872d3aac76edd78caf112cbd5b373b7fd3f11db2
Peter <guanwei0702@gmail.com>
2022-09-11 20:38:21 +0800

    add: index.html RWD 初版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,11 +3,14 @@ $(function() {
 });
 
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
+  
+  slidesPerView: 1,
+  spaceBetween: 8,
+  autoHeight: true,
   loop: true,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 3500,
     disableOnInteraction: false,
   },
   pagination: {
@@ -19,3 +22,4 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+AOS.init();
